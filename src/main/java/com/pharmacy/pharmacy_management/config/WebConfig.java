83d43ba2a379829f230652759d5c4689d2baf69a
Anuradhadhane <anuradhadhane15@gmail.com>
2026-01-08 10:57:20 +0530

    saved file webconfig

--- src/main/java/com/pharmacy/pharmacy_management/config/WebConfig.java
+++ src/main/java/com/pharmacy/pharmacy_management/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://your-app-name.netlify.app")
+                .allowedOrigins("https://pharmasoftfrontend.netlify.app")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*");
     }
